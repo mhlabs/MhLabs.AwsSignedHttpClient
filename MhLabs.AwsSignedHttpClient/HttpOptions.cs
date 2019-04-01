c48393be13cbb49fe88a02823203380b9a22fd07
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2019-04-01 17:12:29 +0200

    chore: disable circuit breaker by default since it disables an entire service and no option to configure it per endpoint, breaking change so up major version number

--- MhLabs.AwsSignedHttpClient/HttpOptions.cs
+++ MhLabs.AwsSignedHttpClient/HttpOptions.cs
@@ -3,7 +3,7 @@ namespace MhLabs.AwsSignedHttpClient
     public class HttpOptions
     {
         public string BaseUrl { get; set; } = null;
-        public bool UseCircuitBreaker { get; set; } = true;
+        public bool UseCircuitBreaker { get; set; } = false;
         public RetryLevel RetryLevel { get; set; } = RetryLevel.Read;
     }
 }

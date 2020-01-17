b07842f090e4e7a6ac16ab568f0c0eea7bd23e53
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2020-01-17 09:55:00 +0100

    fix: guard against null ICredentialProvider and request body

--- MhLabs.AwsSignedHttpClient/Extensions/HttpRequestMessageExtensions.cs
+++ MhLabs.AwsSignedHttpClient/Extensions/HttpRequestMessageExtensions.cs
@@ -9,12 +9,23 @@ namespace MhLabs.AwsSignedHttpClient
     {
         internal static async Task SignRequest(this HttpRequestMessage request, ICredentialsProvider credentialsProvider, string region)
         {
-            var body = (request.Method == HttpMethod.Post || request.Method == HttpMethod.Put || request.Method == HttpMethod.Delete || request.Method == new HttpMethod("PATCH")) ? await request.Content.ReadAsByteArrayAsync() : null;
+            var body = VerbCanContainBody(request) ?
+                await request.Content?.ReadAsByteArrayAsync() :
+                null;
+
+            if (credentialsProvider == null)
+                throw new Exception("CredentialsProvider is null. Probably because it is not registered in IoC");
+
             var credentials = credentialsProvider.GetCredentials();
             if (credentials == null)
                 throw new Exception("Unable to retrieve credentials required to sign the request.");
 
             SignV4Util.SignRequest(request, body, credentials, region, "execute-api");
+        }
+
+        private static bool VerbCanContainBody(HttpRequestMessage request)
+        {
+            return request.Method == HttpMethod.Post || request.Method == HttpMethod.Put || request.Method == HttpMethod.Delete || request.Method == new HttpMethod("PATCH");
         }
     }
 }

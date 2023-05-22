c9ec4be4f6368780e4630608a1c446b74340f5ce
Adnan Gohar <adnangohar@gmail.com>
2023-05-22 22:39:13 +0200

    feat: added a separate AWS request signing message handler with new signing implementation (#15)
    * feat: added a separate AWS request signing message handler with new signing implemenation
    
    * fix: updated package version
    
    * feat: added AdMhHttpClient extension method which only has Tracing message handler registered
    
    * updated package version
    
    ---------
    
    Co-authored-by: Adnan Gohar <adnan.gohar@mathem.se>

--- MhLabs.AwsSignedHttpClient/Extensions/HttpClientExtensions.cs
+++ MhLabs.AwsSignedHttpClient/Extensions/HttpClientExtensions.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace MhLabs.AwsSignedHttpClient
---
+++ MhLabs.AwsSignedHttpClient/MessageHandlers/TracingMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace MhLabs.AwsSignedHttpClient.MessageHandlers
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class TracingMessageHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken)
+        {
+            if (!httpRequestMessage.Headers.Contains(CorrelationHelper.CorrelationIdHeader))
+            {
+                httpRequestMessage.Headers.Add(CorrelationHelper.CorrelationIdHeader, CorrelationHelper.CorrelationId ?? Guid.NewGuid().ToString());
+            }
+
+            var xRayEnvVar = Environment.GetEnvironmentVariable("_X_AMZN_TRACE_ID");
+            if (!string.IsNullOrWhiteSpace(xRayEnvVar))
+            {
+                httpRequestMessage.Headers.Add("X-Amzn-Trace-Id", xRayEnvVar);
+            }
+
+            return await base.SendAsync(httpRequestMessage, cancellationToken);
+        }
+    }
+}
